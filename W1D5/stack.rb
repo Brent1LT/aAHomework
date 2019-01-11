18b66c71831649f965d4108516e162f399da3c3d
Brent Bumann <brent.bumann@aol.com>
2019-01-11 00:39:50 -0800

    Create stack

---
+++ W1D5/stack.rb
@@ -0,0 +1,20 @@
+class Stack
+  attr_reader :stack
+  def initialize
+    @stack = []
+  end
+
+  def push(el)
+    stack << el
+  end
+
+  def pop
+    el = stack[-1]
+    stack = stack[0...-1]
+    el
+  end
+
+  def peek
+    stack[-1]
+  end
+end
